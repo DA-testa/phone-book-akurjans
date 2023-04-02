dad654e71fedc1f147d8bf1d8323a3dd8c03ff5a
akurjans <118132555+akurjans@users.noreply.github.com>
2023-04-03 00:58:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,34 @@
 class Query:
     def __init__(self, query):
         self.type = query[0]
-        self.number = int(query[1])
+        self.number = query[1]
         if self.type == 'add':
             self.name = query[2]
 
 def read_queries():
-    n = 3
-    return [Query(['add', 123456, 'Alice']),
-            Query(['find', 123456]),
-            Query(['del', 123456])]
+    n = int(input())
+    return [Query(input().split()) for i in range(n)]
 
 def write_responses(result):
     print('\n'.join(result))
 
 def process_queries(queries):
     result = []
-    contacts = []
-    
+    # Keep a dictionary of all existing (i.e., not deleted yet) contacts.
+    phone_book = {}
     for cur_query in queries:
-        
         if cur_query.type == 'add':
             
-            for contact in contacts:
-                
-                if contact.number == cur_query.number:
-                    contact.name = cur_query.name
-                    break
-            else: 
-                
-                contacts.append(cur_query)
+            phone_book[cur_query.number] = cur_query.name
         elif cur_query.type == 'del':
             
-            for j in range(len(contacts)):
-                
-                if contacts[j].number == cur_query.number:
-                    contacts.pop(j)
-                    break
+            phone_book.pop(cur_query.number, None)
         else:
             
-            response = 'not found'
-            
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    response = contact.name
-                    break
+            response = phone_book.get(cur_query.number, 'not found')
             result.append(response)
     return result
 
 if __name__ == '__main__':
-    
     write_responses(process_queries(read_queries()))
 
